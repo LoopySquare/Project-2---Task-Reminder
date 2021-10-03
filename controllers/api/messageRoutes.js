6f468bf153187214da4076b2343aab693cf65873
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-10-03 16:07:45 -0600

    added route to handle creating new message

--- controllers/api/messageRoutes.js
+++ controllers/api/messageRoutes.js
@@ -18,16 +18,46 @@ router.get('/:id', withAuth, async (req, res) => {
   }
 });
 
-router.post('/api/message', withAuth, async (req, res) => {
+
+
+router.post('/', withAuth, async (req, res) => {
   try {
     const newMessage = await Message.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
+    console.log(newMessage);
+
     res.status(200).json(newMessage);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+
+    console.log(req.params.id);
+    const messageData = await Message.update({ 
+      event_name: req.body.event_name ,
+      description: req.body.description ,
+      content: req.body.content ,
+      send_date: req.body.send_date ,
+      send_time: req.body.send_time ,
+      am_pm: req.body.am_pm,
+    },
+    {
+      where: {
+        id: req.params.id,
+        // user_id: req.session.user_id
+      }
+    })
+    res.status(200).json(messageData);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
@@ -47,32 +77,6 @@ router.delete('/:id', withAuth, async (req, res) => {
 
     res.status(200).json(messageData);
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
-  try {
-
-    console.log(req.params.id);
-    const messageData = await Message.update({ 
-      event_name: req.body.event_name ,
-      description: req.body.description ,
-      content: req.body.content ,
-      send_date: req.body.send_date ,
-      send_time: req.body.send_time ,
-      am_pm: req.body.ampm,
-    },
-    {
-      where: {
-        id: req.params.id,
-        // user_id: req.session.user_id
-      }
-    })
-    res.status(200).json(messageData);
-  } catch(err) {
     console.log(err);
     res.status(500).json(err);
   }
