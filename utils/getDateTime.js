c04d8f76646b09400e665e8f2689a46376959d9b
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-10-05 13:35:49 -0600

    Updated function to return current_date also

--- utils/getDateTime.js
+++ utils/getDateTime.js
@@ -1,29 +1,36 @@
 
 const dateTime = () => {
   let now = new Date();
+  let date = now.getDate();
+  let month = now.getMonth();
+  let year = now.getFullYear();
   let hours = now.getHours();
   let minutes = now.getMinutes();
-  
+  // Must be done before Formatting of hours variable
   let ampm = hours >= 12 ? 'PM' : 'AM';
-  
+
+  // Format Date and Month for consumption
+  date = date < 10 ? '0' + date : date;
+  month = month + 1;
+
+  // Format Hours into 12 Hour Format
   hours = hours % 12;
   hours = hours ? hours : 12;
+  hours = hours < 10 ? '0' + hours : hours;
+  
+  // Format Minutes from M to MM
   minutes = minutes < 10 ? '0' + minutes : minutes;
 
-  let timeStamp = ''
-  
-  if(hours.length = 1){
-    timeStamp = `0${hours}:${minutes}`
-  } else {
-    timeStamp = `${hours}:${minutes}`
-  }
+  let currentDate = `${year}-${month}-${date}`;
+  let currentTime = `${hours}:${minutes}`
+
 
   let timeObj = {
-    current_time: timeStamp,
+    current_date: currentDate,
+    current_time: currentTime,
     am_pm: ampm
   }
 
-  console.log(timeObj);
   return timeObj
 }
 
