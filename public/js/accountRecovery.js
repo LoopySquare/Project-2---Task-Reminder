73de6f4e991ab37dec4570e6978f8696b109135b
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-10-07 00:33:32 -0600

    Added logic to recover account and push to password reset api

---
+++ public/js/accountRecovery.js
@@ -0,0 +1,50 @@
+const recoverFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email').value.trim();
+
+  validEmail = await validateEmail(email);
+  
+  if (email) {
+
+    const response = await fetch(`/api/users/account/recovery/`, {
+      method: 'POST',
+      body: JSON.stringify({ email }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    console.log(response);
+
+    if (response.ok) {
+      document.location.replace(`/account/password/reset`);
+    } else {
+      swal.fire("Current Password Does not match what's on record");
+      document.querySelector('#current-password').focus();
+      document.getElementById("current-password").classList.add('is-danger');
+    }
+  }
+};
+
+const validateEmail = async (email) => {
+
+  let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if(!email.match(mailFormat)) {
+    swal.fire("Please Enter a valid Email!");
+    document.getElementById("email").classList.add('is-danger');
+    document.getElementById("invalidEmail").classList.remove('is-hidden');
+
+     return false;
+  } else {
+    document.getElementById("email").classList.remove('is-danger');
+    document.getElementById("invalidEmail").classList.add('is-hidden');
+    return true;
+  }
+
+}
+
+
+document
+  .querySelector('#recover-account')
+  .addEventListener('click', recoverFormHandler);
