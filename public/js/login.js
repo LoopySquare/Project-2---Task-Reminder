f3dd196cb42545c83723c604696fc8dea5aec741
LoopySquare <tjobin1@gmail.com>
2021-10-06 20:19:17 -0600

    added/updated: logo, validation inside the form for creating an account, linked custom sass with bulma, all views for html sample pages

--- public/js/login.js
+++ public/js/login.js
@@ -19,7 +19,7 @@ const loginFormHandler = async (event) => {
       // If successful, redirect the browser to the profile page
       document.location.replace('/profile');
     } else {
-      alert(response.statusText);
+      document.getElementById("").classList.add('MyClass');
     }
   }
 };
