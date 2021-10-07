95d148c4c662a7c573a10a0fd1264bd8e2cec124
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-10-06 20:40:22 -0600

    built js to handle form submission

--- public/js/editAccount.js
+++ public/js/editAccount.js
@@ -1,49 +1,128 @@
-const editButtonHandler = async (event) => {
-    event.preventDefault ();
 
-    const id = event.target.getAttribute('profile-id');
+const saveButtonHandler = async (event) => {
+  event.preventDefault ();
 
-     // Collect values from the login form
+  const id = event.target.getAttribute('data-editId');
 
-     const first_name = document.querySelector('#first-name').value.trim();
-     const last_name = document.querySelector('#last-name').value.trim();
-     const password = document.querySelector('#password').value.trim();
-     const confirmPassword = document.querySelector('#confirm-password').value.trim();
-     const email = document.querySelector('#email').value.trim();
-     const rawPhone = document.querySelector('#phone').value.trim();
+  console.log(id);
 
-     if (first_name && last_name && password && confirmPassword && email && rawPhone) {
+  // Collect values from the login form
 
-        const response = await fetch(`/api/users/create/${id}`, {
-            method: 'POST',
-            body: JSON.stringify({ first_name, last_name, email, password, phone }),
-            headers: { 'Content-Type': 'application/json' },
-          });
-      
-        if (response.ok) {
-        // If successful, redirect the browser to the profile page
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }  
-    }
-        
-};
-const cancelButtonHandler = async (event) => {
-    event.preventDefault();
-  
-    const cancel = confirm("Are you sure you want to cancel?\n All changes will not be saved.")
-  
-    if(cancel){
-      document.location.replace(`/profile`);
+  const first_name = document.querySelector('#first-name').value.trim();
+  const last_name = document.querySelector('#last-name').value.trim();
+  const phone = formatPhone(document.querySelector('#phone').value.trim());
+
+  console.log(phone.length);
+
+  const validName = await validateName(first_name, last_name);
+  const validPhone = await validatePhone(phone);
+
+  console.log(validPhone);
+
+  if(!validName){
+    return;
+  }
+
+  if(!validPhone){
+    return;
+  } 
+
+  if (first_name && last_name && phone && validName && validPhone) {
+
+    console.log('Sending Fetch');
+    const response = await fetch(`/api/users/account/edit/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ first_name, last_name, phone }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
     } else {
-      return;
-    }
+      alert(response.statusText);
+    }  
+  }   
 };
+
+
+const cancelButtonHandler = async (event) => {
+  event.preventDefault();
+
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'You will be returned to your Account Page.',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, cancel!',
+    cancelButtonText: 'No, I will complete'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.location.replace(`/profile`);
+    // For more information about handling dismissals please visit
+    // https://sweetalert2.github.io/#handling-dismissals
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+      return
+    }
+  })
+};
+
+const updatePasswordHandler = async () => {
+  
+  document.location.replace(`/account/password/update`);
+
+};
+
+const validateName = async (fname, lname) => {
+
+  if(fname == '' && lname == ''){
+    swal.fire("Please Enter your Name");
+    document.querySelector('#first-name').focus();
+    return false;
+  }
+
+  if(fname == ''){
+    swal.fire("Please Enter your First Name");
+    document.querySelector('#first-name').focus();
+    return false;
+  }
+
+  if(lname == '' ){
+    swal.fire("Please Enter your Last Name");
+    document.querySelector('#last-name').focus();
+    return false;
+  }
+
+  return true;
+
+}
+
+const formatPhone = (phone) => {
+
+  return phone.replace(/\D/g, '');
+}
+
+const validatePhone = async (phone) => {
+
+  if(phone === '' || phone.length !== 10){
+    swal.fire("Please Enter a 10 Digit Phone Number");
+    document.querySelector('#phone').focus();
+    return false;
+  }
+    return true;
+}
+
+
 document
   .querySelector('#save-profile')
-  .addEventListener('click', editButtonHandler);
+  .addEventListener('click', saveButtonHandler);
 
 document
   .querySelector('#cancel')
   .addEventListener('click', cancelButtonHandler);
+
+document
+  .querySelector('#update-password')
+  .addEventListener('click', updatePasswordHandler);
+
+
