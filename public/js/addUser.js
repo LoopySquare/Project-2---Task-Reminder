b7481276f49a3e3b17b054ad7fa35a763b6395f8
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-10-06 20:40:03 -0600

    Added sweetalert2 alerts

--- public/js/addUser.js
+++ public/js/addUser.js
@@ -1,8 +1,6 @@
 const createFormHandler = async (event) => {
   event.preventDefault();
 
-  console.log('I clicked this button');
-
   // Collect values from the login form
   const first_name = document.querySelector('#first-name').value.trim();
   const last_name = document.querySelector('#last-name').value.trim();
@@ -11,30 +9,38 @@ const createFormHandler = async (event) => {
   const email = document.querySelector('#email').value.trim();
   const rawPhone = document.querySelector('#phone').value.trim();
 
-  let passMatch = validatePass(password, confirmPassword);
-  let validEmail = validateEmail(email);
-  let phone = formatPhone(rawPhone);
+  const validName = await validateName(first_name, last_name)
+  const passMatch = await validatePass(password, confirmPassword);
+  const validEmail = await validateEmail(email);
+  const phone = await formatPhone(rawPhone);
+  const validPhone = await validatePhone(phone)
 
+
+  if(!validName){
+    blankPass();
+    return;
+  }
+  
   if(!validEmail){
     document.querySelector('#email').focus();
     blankPass();
-    return
-  }
+    return;
+  } 
 
-  if(!passMatch) {
+   if(!passMatch) {
     blankPass();
     document.querySelector('#password').focus();
-    return
-  }
-
-  if(phone.length !== 10){
+    return;
+  } 
+  
+  if(!validPhone){
     blankPass();
     document.querySelector('#phone').focus();
-    alert("Please enter a valid 10 digit Phone Number")
-    return
-  }
-
+    return;
+  } 
+  
   if (first_name && last_name && email && password && phone) {
+
     const response = await fetch('/api/users/create', {
       method: 'POST',
       body: JSON.stringify({ first_name, last_name, email, password, phone }),
@@ -49,29 +55,59 @@ const createFormHandler = async (event) => {
   }
 };
 
+const validateName = async (fname, lname) => {
+
+  if(fname == '' && lname == ''){
+    swal.fire("Please Enter your Name");
+    document.querySelector('#first-name').focus();
+    return false;
+  }
+
+  if(fname == ''){
+    swal.fire("Please Enter your First Name");
+    document.querySelector('#first-name').focus();
+    return false;
+  }
+
+  if(lname == '' ){
+    swal.fire("Please Enter your Last Name");
+    document.querySelector('#last-name').focus();
+    return false;
+  }
+
+  return true;
+
+}
+
 // VALIDATE PASSWORD AND CONFIRM PASSWORD ARE THE SAME AND PROPER LEN
-const validatePass = (pass, confirmPass) => {
+const validatePass = async (pass, confirmPass) => {
+
+  if(pass === ''){
+    swal.fire("Please Enter a Password");
+    return false;
+  }
 
   if(pass.length < 8){
-    alert("Password must be more than 8 Characters");
+    swal.fire("Password must be more than 8 Characters");
     return false;
   }
 
   if(pass !== confirmPass) {
-    alert("Passwords did not match");
+    swal.fire("Passwords did not match");
     return false;
   } else {
     return true;
   }
+
 }
 
 // VALIDATE EMAIL IS IN AN EMAIL FORMAT
-const validateEmail = (email) => {
+const validateEmail = async (email) => {
 
   let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   if(!email.match(mailFormat)) {
-     alert("You have entered an invalid email address!")
+    swal.fire("You have entered an invalid email address!")
      return false;
   } else {
     return true;
@@ -80,6 +116,16 @@ const validateEmail = (email) => {
 }
 
 // STRIP DASHES FROM PHONE FIELDS
+
+const validatePhone = async (phone) => {
+  if(phone === '' || phone.length !== 10){
+    swal.fire("Please Enter a 10 Digit Phone Number");
+    return false;
+  } else {
+    return true;
+  }
+}
+
 const formatPhone = (phone) => {
 
   return phone.replace(/\D/g, '');
